d8bb87703f1ca15d1b1366413db44d83e41deb36
Shenaeva <56427199+Shenaeva@users.noreply.github.com>
2021-02-25 15:51:19 +0300

    + manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+import ry.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    public Post[] search(int id, int fromId, String text) { //поиск по id поста, id автора поста, по текста
+        return posts;
+    }
+
+    public void delete(int id, int ownerId) { // удаление записи на стене
+
+    }
+
+
+}
